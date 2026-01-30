ecd81d23eead2e9d85907ac790350c142060d413
TaintedWhite <florin.lupas.business@gmail.com>
2026-01-30 15:01:55 +0000

    Refurbished SpriteRandomizerLibrary and integrated it into demo game
    It now instantiates and returns a GameObject when called.
    
    yea

--- Assets/Scripts/NPCManager.cs
+++ Assets/Scripts/NPCManager.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class NPCManager : MonoBehaviour
 {
     public GameObject refugeePrefab;
+    public GameObject refugeeMaskPrefab;
 
     public Transform spawnRight;
     public Transform standPoint;
@@ -19,14 +20,27 @@ public class NPCManager : MonoBehaviour
 
     void SpawnRefugee()
     {
+        GameObject ref_obj = Instantiate(refugeePrefab, spawnRight.position, Quaternion.identity);
+        GameObject ref_mask = GetComponent<SpriteRandomizerLibrary>().InstantiateRandomMask(refugeeMaskPrefab, ref_obj, (spawnRight.position + new Vector3(0,.50f,0)));
+        currentRefugee = ref_obj.GetComponent<Refugee>();
+
+        currentRefugee.MoveTo(standPoint.position);
+
+
+        /* DEPRECATED / OLD
         GameObject obj = Instantiate(refugeePrefab,spawnRight.position,Quaternion.identity);
         currentRefugee = obj.GetComponent<Refugee>();
-
+        GetComponent<SpriteRandomizerLibrary>().InstantiateRandomMask(refugeeMaskPrefab, obj,new Vector3(0,0,0));
+        refugeeMaskPrefab.gameObject.transform.position = new Vector3(0,0,0);
+        currentRefugee.MoveTo(standPoint.position);
+        */
+        /* DEPRECATED / OLD
         // Assign random mask
         MaskData randomMask = possibleMasks[Random.Range(0, possibleMasks.Length)];
         currentRefugee.SetMask(randomMask);
 
         currentRefugee.MoveTo(standPoint.position);
+        */
     }
 
 
